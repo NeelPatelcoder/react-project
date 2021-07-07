d8997349328c8944e67ea68069a545ab4ef417aa
NeelPatelcoder <neelp.mobio@gmail.com>
2021-07-07 12:41:09 +0530

    Another Breanch check it

---
+++ src/NewExpense/NewExpense.js
@@ -0,0 +1,19 @@
+import './NewExpense.css';
+import React from "react";
+
+import ExpenseForm from './ExpenseForm.js';
+
+const NewExpense = () =>{
+
+
+
+    return(
+        <div className='new-expense'>
+            <ExpenseForm />
+        </div>
+    );
+
+;}
+
+
+export default NewExpense;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+
+  const [title,setTitle] = useState(props.title);
+  console.log('Expensesitem evaluted');
+
+  const clickHandler = () => {
+    setTitle("updated!");
+    console.log(title);
+  };
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Add Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
