707026a8583bbd40a0c6547022a18046357c9ce5
NeelPatelcoder <neelp.mobio@gmail.com>
2021-07-09 12:04:02 +0530

    This is form submit page

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from './ExpensesDate';
 import Card from '../UI/Card';
-import './ExpenseItem.css';
+import './ExpensesItem.css';
 
 const ExpenseItem = (props) => {
-
-  const [title,setTitle] = useState(props.title);
-  console.log('Expensesitem evaluted');
-
-  const clickHandler = () => {
-    setTitle("updated!");
-    console.log(title);
-  };
-
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Add Title</button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/ExpensesDate.js
+++ src/components/Expenses/ExpensesDate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './ExpenseDate.css';
+import './ExpensesDate.css';
 
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
